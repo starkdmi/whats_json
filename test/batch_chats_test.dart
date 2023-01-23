2854a0a338ec61cb5964163a9adf63c04255d76d
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-01-24 01:28:10 +0300

    Update batched test with native isolates

--- test/batch_chats_test.dart
+++ test/batch_chats_test.dart
@@ -3,14 +3,12 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:test/test.dart';
 import 'package:whats_json/whats_json.dart';
-import 'package:compute/compute.dart';
+import 'dart:isolate';
 
 void main() {
   group("Test all unique chats (batched)", () {
     final path = "./test/data/chats_unique";
     final directory = Directory(path);
-
-    // tearDown(() => exit(0)); // finish isolates if any (bug in compute method)
 
     test("Parse all unique chats without failures, timeouts, non-empty and with valid dates", () async {
       int errors = 0;
@@ -23,44 +21,39 @@ void main() {
         
         // skip unsupported files (Arabic RTL)
         if ([
-          "$path/WhatsApp Chat - Elon Musk (Arabic)", 
-          "$path/WhatsApp Chat - Arabic_NoMedia Dmitry S"
+          "$path/WhatsApp Chat - Elon Musk (Arabic).txt", 
+          "$path/WhatsApp Chat - Arabic_NoMedia Dmitry S.txt",
         ].contains(entry.path)) continue;
-
-        final file = File(entry.path);
 
         try {
           // print("${entry.path}\n");
 
-          final stream = file
-            .openRead()
-            .transform(const Utf8Decoder())
-            .transform(const LineSplitter());
+          // TODO: perfomance may be improved by re-using a single or multiple isolates and not spawning new each time
+          Isolate? isolate;
+          await runZonedGuarded(() async {
+            final port = ReceivePort();
+            isolate = await Isolate.spawn(parseIsolated, [port.sendPort, entry.path]);
+            Iterable<Map<String, dynamic>>? messages = await port.first.timeout(Duration(seconds: 10), onTimeout: () => null) as Iterable<Map<String, dynamic>>?;
+            if (messages == null) {
+              print("Timeout ${entry.path}\n");
+              timeouts++;
+              return;
+            } 
+            if (messages.length < 2) {
+              print("${messages.length}: ${entry.path}\n");
+              lessThanTwoMessages++;
+            }
 
-            await runZonedGuarded(() async {
-              TimeoutExecuter api = TimeoutExecuter();
-              await api.parseWithTimeout(stream: stream, timeout: Duration(seconds: 20)).then((messages) {
-                if (messages == null) {
-                  print("Timeout: ${entry.path}\n");
-                  // throw "Timeout: ${entry.path}\n";
-                  timeouts++;
-                } else {              
-                  if (messages.length < 2) {
-                    print("${messages.length}: ${entry.path}\n");
-                    lessThanTwoMessages++;
-                  }
-
-                  if (messages.any((m) => m["date"] == null || m["date"] == 0)) {
-                    print("Empty dates: ${entry.path}\n");
-                    // throw "Empty dates: ${entry.path}\n";
-                    invalidDates++;
-                  }
-                }
-              });
-            }, (error, _) {
-              print("$error ${entry.path}\n");
-              errors++;
-            });
+            if (messages.any((m) => m["date"] == null || m["date"] == 0)) {
+              print("Empty dates: ${entry.path}\n");
+              // throw "Empty dates: ${entry.path}\n";
+              invalidDates++;
+            }
+          }, (error, _) {
+            isolate?.kill();
+            print("$error ${entry.path}\n");
+            errors++;
+          });
         } catch (error) {
           print("$error ${entry.path}\n");
           errors++;
@@ -71,37 +64,19 @@ void main() {
       expect(timeouts, equals(0));
       expect(lessThanTwoMessages, equals(0));
       expect(invalidDates, equals(0));
-    });
+    }, timeout: Timeout(Duration(minutes: 10)));
   });
 }
 
-class TimeoutExecuter {
-  late Completer<List<Map<String, dynamic>>?> _completer;
-  Timer? _timer;
+Future<void> parseIsolated(List<dynamic> args) async {
+  SendPort port = args.first as SendPort;
+  String path = args.last as String;
 
-  Future<List<Map<String, dynamic>>?> parseWithTimeout({ required Stream<String> stream, Duration timeout = const Duration(seconds: 10) }) async {
-    _completer = Completer<List<Map<String, dynamic>>?>();
+  final stream = File(path)
+    .openRead()
+    .transform(const Utf8Decoder())
+    .transform(const LineSplitter());
 
-    compute(whatsAppGetMessages, stream).then((response) {
-      if (_completer.isCompleted == false) {
-        _timer?.cancel();
-        _completer.complete(response.toList());
-      }
-    });
-
-    _timer = Timer(timeout, () {
-      if (_completer.isCompleted == false) {
-        _completer.complete(null);
-      }
-    });
-
-    return _completer.future;
-  }
-
-  void cancelOperation() {
-    _timer?.cancel();
-    if (_completer.isCompleted == false) {
-      _completer.complete(null);
-    }
-  }
+  final messages = await whatsAppGetMessages(stream, skipSystem: true);
+  Isolate.exit(port, messages);
 }
