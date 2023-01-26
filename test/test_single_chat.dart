d08e95da6fed6bbd156b336c28e350d91f76c786
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2023-01-26 20:21:08 +0300

    Update test_single_chat.dart

--- test/test_single_chat.dart
+++ test/test_single_chat.dart
@@ -2,9 +2,17 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:whats_json/whats_json.dart';
 
+// Lint
+//  dart format .
+// Pub dev analytics
+//  pana .
+// Code coverage
+//  flutter test --coverage
+//  genhtml coverage/lcov.info -o coverage/html
+
 void main() async {
   // file location
-  // final path = "./test/data/chats_unique/WhatsApp Chat - Elon Musk (EN+Media).txt";
+  final path = "./test/data/chats_unique/WhatsApp Chat - Elon Musk (EN+Media).txt";
   // final path = "./test/data/chats_unique/WhatsApp Chat - Dmitry S.txt";
   // final path = "./test/data/calendars/Japanese iOS.txt";
   // final path = "./test/data/calendars/Buddhist iOS.txt";
@@ -12,8 +20,7 @@ void main() async {
 
   // final path = "./test/data/chats_unique/WhatsApp Chat - Arabic_NoMedia Dmitry S.txt"; // arabic dates
   // final path = "./test/data/chats_unique/WhatsApp Chat - Elon Musk (Arabic).txt"; // international dates
-  final path =
-      "./test/data/chats_unique/WhatsApp Chat - Elon Musk (Arabic+Media).txt"; // international + media
+  // final path = "./test/data/chats_unique/WhatsApp Chat - Elon Musk (Arabic+Media).txt"; // international + media
 
   // load file
   final stream = File(path)
@@ -23,7 +30,7 @@ void main() async {
 
   // get messages
   final messages = await whatsAppGetMessages(stream,
-      skipSystem: true, logger: SimpleLogger());
+      skipSystem: false, logger: SimpleLogger());
   print("messages count: ${messages.length}");
 
   // pring messages
